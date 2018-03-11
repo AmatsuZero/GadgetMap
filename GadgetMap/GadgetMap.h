6d4df924e9a0f8081b159887f1e51494ce95a8cf
Daubert <jzh16s@hotmail.com>
2018-03-11 23:36:11 +0800

    Initial Commit

---
+++ GadgetMap/GadgetMap.h
@@ -0,0 +1,19 @@
+//
+//  GadgetMap.h
+//  GadgetMap
+//
+//  Created by modao on 2018/3/11.
+//  Copyright © 2018年 MockingBot. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for GadgetMap.
+FOUNDATION_EXPORT double GadgetMapVersionNumber;
+
+//! Project version string for GadgetMap.
+FOUNDATION_EXPORT const unsigned char GadgetMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GadgetMap/PublicHeader.h>
+
+
